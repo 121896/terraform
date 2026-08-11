10d4cb3c41d78ac6f3345003ab2c474dde78bcb6
121896 <cab2760@gmail.com>
2026-08-11 16:40:00 +0900

    3rd commit

---
+++ 02/lab01/main2.tf
@@ -0,0 +1,81 @@
+#
+# EC2(SG, user_data(web server), keypair) 생성
+#
+# * SG(22/tcp, 80/tcp, 443/tcp)
+# * keypair
+# * EC(user_data(web server))
+
+# 1) SG(22/tcp, 80/tcp, 443/tcp)
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "mySG" {
+  name        = "mySG"
+  description = "Allow SSH,WEB inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myVPC.id
+
+  tags = {
+    Name = "mySG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_22" {
+  security_group_id = aws_security_group.mySG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_80" {
+  security_group_id = aws_security_group.mySG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_443" {
+  security_group_id = aws_security_group.mySG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.mySG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# 2) keypair
+# CMD: ssh keygen -t rsa -N "" ~/.ssh/mykeypair
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
+resource "aws_key_pair" "mykeypair" {
+  key_name   = "mykeypair"
+  public_key = file("~/.ssh/mykeypair.pub")
+}
+
+# 3) EC2(user_data(web server))
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+# * 새로 생성된 Subnet(myPubSN)에서 EC2 인스턴스 생성 - subnet_id
+# * 새로 만든 mykeypair를 사용하여 EC2 인스턴스 생성 - key_name
+# * 새로 만든 mySG를 사용하여 EC2 인스턴스 생성 - vpc_security_group_ids
+# * user_data -> user_data_replace_on_change
+resource "aws_instance" "myEC2" {
+  ami                         = "ami-048f644e868baa0e8"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.myPubSN.id
+  key_name                    = "mykeypair"
+  vpc_security_group_ids      = [aws_security_group.mySG.id]
+  user_data_replace_on_change = true
+  user_data                   = <<-EOF
+        #!/bin/bash
+        dnf install -y httpd mod_ssl
+        echo "My Web Server Test Page" > /var/www/html/index.html
+        systemctl enable --now httpd
+        EOF
+
+  tags = {
+    Name = "myEC2"
+  }
+}
---
+++ 02/lab01/main3.tf
@@ -0,0 +1,132 @@
+# 작업 계획
+# * NAT Gateway 생성(Public Subnet)
+# * Private Subnet 생성
+# * Private Route Table 생성 및 설정
+# * SG 생성
+# * EC2 생성
+
+# 1) NAT Gateway 생성(Public Subnet)
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/nat_gateway
+# * EIP 생성
+# * Public Subnet에 NAT Gateway 생성
+resource "aws_eip" "myEIP" {
+  domain = "vpc"
+  #depends_on                = [aws_internet_gateway.gw]
+
+  tags = {
+    Name = "myEIP"
+  }
+}
+
+resource "aws_nat_gateway" "myNATGW" {
+  allocation_id = aws_eip.myEIP.id
+  subnet_id     = aws_subnet.myPubSN.id
+
+  tags = {
+    Name = "myNATGW"
+  }
+
+  depends_on = [aws_internet_gateway.myIGW]
+}
+
+# 2) Private Subnet 생성
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+# * 새로 생성된 myVPC에 Private Subnet 생성
+resource "aws_subnet" "myPriSN" {
+  vpc_id     = aws_vpc.myVPC.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "myPriSN"
+  }
+}
+
+# 3) Private Route Table 생성 및 설정
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table" "myPriRT" {
+  vpc_id = aws_vpc.myVPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.myNATGW.id
+  }
+
+  tags = {
+    Name = "myPriRT"
+  }
+}
+
+resource "aws_route_table_association" "myPriRTassoc" {
+  subnet_id      = aws_subnet.myPriSN.id
+  route_table_id = aws_route_table.myPriRT.id
+}
+
+# 4) SG 생성
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+# * SG(22/tcp, 80/tcp, 443/tcp)
+resource "aws_security_group" "mySG2" {
+  name        = "mySG2"
+  description = "Allow SSH,WEB inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.myVPC.id
+
+  tags = {
+    Name = "mySG2"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_22-2" {
+  security_group_id = aws_security_group.mySG2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_80-2" {
+  security_group_id = aws_security_group.mySG2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_443-2" {
+  security_group_id = aws_security_group.mySG2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all-2" {
+  security_group_id = aws_security_group.mySG2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+# 5) EC2 생성
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+# * 새로 생성된 SG에서 EC2 인스턴스 생성 - vpc_security_group_ids
+# * 새로 생성된 myPriSN에서 EC2 인스턴스 생성 - subnet_id
+# * mykeypair - key_name
+# * user_data -> user_data_replace_on_change
+resource "aws_instance" "myEC2-2" {
+  ami                         = "ami-048f644e868baa0e8"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.myPriSN.id
+  key_name                    = "mykeypair"
+  vpc_security_group_ids      = [aws_security_group.mySG2.id]
+  user_data_replace_on_change = true
+  user_data                   = <<-EOF
+        #!/bin/bash
+        dnf install -y httpd mod_ssl
+        echo "My Web Server Test Page" > /var/www/html/index.html
+        systemctl enable --now httpd
+        EOF
+
+  tags = {
+    Name = "myEC2-2"
+  }
+}
